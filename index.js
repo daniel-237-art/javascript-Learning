ec7593164569b6d2e8c659533d49cb55fb02a06c
Dañel7 <bonyohedaniel@gmail.com>
2026-05-07 03:01:52 +0100

    Added the color animation and playlist

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const wrapper = document.querySelector(".wrapper"),
-    musingImg = wrapper.querySelector(".img-area img"),
+    musicImg = wrapper.querySelector(".img-area img"),
     musicName = wrapper.querySelector(".song-details .name"),
     musicArtist = wrapper.querySelector(".song-details .artist"),
     playPauseBtn = wrapper.querySelector(".play-pause"),
@@ -20,37 +20,43 @@ const allMusic = [
         name: "01 - Hikaru Nara",
         artist: "Goose house",
         img: "dems (1)", // This looks for image/dems (1).jpg
-        src: "song1.mp3"  // This looks for music/dems (1).mp3
+        src: "song1.mp3",  // This looks for music/dems (1).mp3
+        gradient: "linear-gradient(135deg, #ff9a9e, #fecfef, #a1c4fd, #c2e9fb)"
     },
     {
         name: "01 - Orange",
         artist: "7!!",
         img: "dems (2)",
-        src: "song5;mp3"
+        src: "song5;mp3",
+        gradient: "linear-gradient(135deg, #ff758c, #ff7eb3, #a18cd1, #fbc2eb)"
     },
     {
         name: "メフィスト",
         artist: "女王蜂",
         img: "dems (3)",
-        src: "song8.mp3"
+        src: "song8.mp3",
+        gradient: "linear-gradient(135deg, #141414, #434343, #eeb9cc, #9a769e)"
     },
     {
         name: "紅蓮華",
         artist: "LiSA",
         img: "dems (4)",
-        src: "song9.mp3"
+        src: "song9.mp3",
+        gradient: "linear-gradient(135deg, #f857a6, #ff5858, #ff0844, #ffb199)"
     },
     {
         name: "竈門禰豆子のうた",
         artist: "椎名豪 featuring 中川奈美",
         img: "dems (5)",
-        src: "song10.mp3"
+        src: "song10.mp3",
+        gradient: "linear-gradient(135deg, #a18cd1, #fbc2eb, #fad0c4, #ffd1ff)"
     },
     {
         name: "セレナーデ",
         artist: "natori",
         img: "dems (6)",
-        src: "song3.flac"
+        src: "song3.flac",
+        gradient: "linear-gradient(135deg, #25974fff, #03108aff, #4facfe, #00f2fe)"
     },
 
 ];
@@ -59,13 +65,17 @@ const allMusic = [
 let musicIndex = 1;
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
+    initPlaylist();
 });
 
 function loadMusic(index) {
     musicName.innerText = allMusic[index - 1].name;
     musicArtist.innerText = allMusic[index - 1].artist;
-    musingImg.src = `image/${allMusic[index - 1].img}.jpg`;
+    musicImg.src = `image/${allMusic[index - 1].img}.jpg`;
     audio.src = `music/${allMusic[index - 1].src}`;
+
+    // On change la variable CSS qui définit la couleur de fond
+    wrapper.style.setProperty('--song-gradient', allMusic[index - 1].gradient);
 }
 
 function playMusic() {
@@ -130,6 +140,33 @@ playPauseBtn.addEventListener("click", () => {
     isMusicPaused ? pauseMusic() : playMusic();
 });
 
+moreMusicBtn.addEventListener("click", () => {
+    musicList.classList.toggle("show");
+});
 
+closeMusicBtn.addEventListener("click", () => {
+    musicList.classList.remove("show");
+});
 
+function initPlaylist() {
+    playlist.innerHTML = "";
+    allMusic.forEach((song, i) => {
+        let liTag = `<li data-index="${i + 1}">
+            <div class="row">
+                <span>${song.name}</span>
+                <p>${song.artist}</p>
+            </div>
+        </li>`;
+        playlist.insertAdjacentHTML("beforeend", liTag);
+    });
 
+    const allLiTags = playlist.querySelectorAll("li");
+    allLiTags.forEach(li => {
+        li.addEventListener("click", () => {
+            musicIndex = parseInt(li.getAttribute("data-index"));
+            loadMusic(musicIndex);
+            playMusic();
+            musicList.classList.remove("show");
+        });
+    });
+}
